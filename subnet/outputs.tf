d048d04541d01869928e12effc9bd536ef1d1520
Jerzy Kopaczewski <jerzy.kopaczewski@yupo.pl>
2016-08-29 17:25:14 +0200

    fix: ouputs as a list

--- subnet/outputs.tf
+++ subnet/outputs.tf
@@ -4,20 +4,20 @@
 
 // The ID of the Subnet
 output "id" {
-    value = "${aws_subnet.main.*.id}"
+    value = [ "${aws_subnet.main.*.id}" ]
 }
 
 // Subnets availability_zones 
 output "availability_zone" {
-    value = "${aws_subnet.main.*.availability_zone}"
+    value = [ "${aws_subnet.main.*.availability_zone}" ]
 }
 
 // Subnets cidr_block 
 output "cidr_block" {
-    value = "${aws_subnet.main.*.cidr_block}"
+    value = [ "${aws_subnet.main.*.cidr_block}" ]
 }
 
 // Subnets vpc_id 
 output "vpc_id" {
-    value = "${aws_subnet.main.*.vpc_id}"
+    value = [ "${aws_subnet.main.*.vpc_id}" ]
 }

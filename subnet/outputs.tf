535d9ea351e318cfffe9931ad294db17777e9fa0
Jerzy Kopaczewski <jerzy.kopaczewski@yupo.pl>
2016-08-29 17:21:46 +0200

    adding outputs

---
+++ subnet/outputs.tf
@@ -0,0 +1,23 @@
+###############################################
+# Module AWS VPC Subnet
+###############################################
+
+// The ID of the Subnet
+output "id" {
+    value = "${aws_subnet.main.*.id}"
+}
+
+// Subnets availability_zones 
+output "availability_zone" {
+    value = "${aws_subnet.main.*.availability_zone}"
+}
+
+// Subnets cidr_block 
+output "cidr_block" {
+    value = "${aws_subnet.main.*.cidr_block}"
+}
+
+// Subnets vpc_id 
+output "vpc_id" {
+    value = "${aws_subnet.main.*.vpc_id}"
+}

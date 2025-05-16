2d87e6fe196cebc81622eb68bdc07bdf52c4e290
Zmicier Kruts <zmicier@Zmiciers-MacBook-Pro.local>
2025-05-16 14:19:25 +0200

    variable type errors in gws-au

--- dhcp_options/variables.tf
+++ dhcp_options/variables.tf
@@ -12,11 +12,11 @@ variable "deploytag" {}
 
 // Optional
 variable domain_name {}
-variable domain_name_servers {
+variable "domain_name_servers" {
 	default = ["127.0.0.1"]
 	type = list(string)
 }
-variable ntp_servers { 
+variable "ntp_servers" { 
 	default = ["127.0.0.1"]
 	type = list(string)
 }
